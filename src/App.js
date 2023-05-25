6db9760ff9cb84bfeeda7e61d22d9105d02f4297
DEVENDRA MESHRAM <120394539+triggerdeva@users.noreply.github.com>
2023-05-25 15:13:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./App.css";
+import "./styles/App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
@@ -12,15 +12,15 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={ProductList} />
-          <Route path="/details" component={Details} />
-          <Route path="/cart" component={Cart} />
-          <Route component={Default} />
-        </Switch>
-        <Modal />
-      </React.Fragment>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={ProductList} />
+        <Route path="/details" component={Details} />
+        <Route path="/cart" component={Cart} />
+        <Route component={Default} />
+      </Switch>
+      <Modal />
+    </React.Fragment>
     );
   }
 }
